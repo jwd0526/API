9472691add5cc8c37e43ba784b584cde33a36f98
Jackson Davis <jwd0526@gmail.com>
2024-02-22 20:57:31 -0500

    Finished scraper

--- src/scraper.ts
+++ src/scraper.ts
@@ -11,39 +11,45 @@ const main = async () => {
   const websiteURL =
     "https://www.sportsbookreview.com/betting-odds/nba-basketball/";
   await website.goto(websiteURL);
-  const rows = await website.$$("#tbody-nba > div");
-  let rowSpreads;
-  let rowNames;
-  let rowLine;
 
-  for (let i = 0; i < rows.length; i++) {
-    rowNames = await website.evaluate(() => {
-      const elements = Array.from(
-        document.getElementsByClassName(
-          "GameRows_adjust__NZn2m GameRows_opener__NivKJ GameRows_opener__NivKJ"
-        )
-      );
-      return elements.map((element) => element.textContent);
-    });
+  let rowData = [];
 
-    rowLine = await website.evaluate(() => {
-      const elements = Array.from(
-        document.getElementsByClassName("GameRows_opener__NivKJ")
-      );
-      return elements.map((element) => element.textContent);
-    });
+  const rowNames = await website.evaluate(() => {
+    const elements = Array.from(
+      document.getElementsByClassName("GameRows_participantBox__0WCRz")
+    );
+    return elements.map((element) => element.textContent);
+  });
 
-    rowSpreads = await website.evaluate(() => {
-      const elements = Array.from(
-        document.getElementsByClassName("GameRows_participantBox__0WCRz")
-      );
-      return elements.map((element) => element.textContent);
+  const rowSpreads = await website.evaluate(() => {
+    const elements = Array.from(
+      document.getElementsByClassName(
+        "GameRows_adjust__NZn2m GameRows_opener__NivKJ"
+      )
+    );
+    return elements.map((element) => element.textContent);
+  });
+
+  const rowLines = await website.evaluate(() => {
+    const elements = document.querySelectorAll(
+      ".GameRows_margin__fN0XT > .GameRows_opener__NivKJ:nth-child(2)"
+    );
+    return Array.from(elements).map((element) => element.textContent);
+  });
+
+  for (let j = 0; j < rowNames.length; j++) {
+    rowData.push({
+      team: rowNames[j],
+      spread: rowSpreads[j],
+      line: rowLines[j],
     });
   }
-  browser.close();
-  console.log(rowSpreads);
-  console.log(rowNames);
-  console.log(rowLine);
-};
 
+  const csvData = rowData
+    .map(({ team, spread, line }) => `${team},${spread},${line}`)
+    .join("\n");
+  fs.writeFileSync("odds.csv", csvData);
+
+  browser.close();
+};
 export default main;
